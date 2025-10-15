0da7ceff5cf1f8a7ea593395d1cbdaee6c15d1e5
Stefan Strandberg <stefan.m.strandberg@hotmail.com>
2025-10-15 18:14:39 +0200

    PublicSealedrecord, Property: JsonPropertyname. Iservices + public const för smhi

---
+++ Domain/Models/MetObsDtos.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace weather_report_smhi.Domain.Models;
+
+public sealed record StationSetDataResponse(
+    [property: JsonPropertyName("key")] string Key,
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("value")] IReadOnlyList<StationData> Value
+);
+
+public sealed record StationData(
+    [property: JsonPropertyName("date")] long DateUnixMs,
+    [property: JsonPropertyName("value")] double? Value,
+    [property: JsonPropertyName("quality")] int? Quality,
+    [property: JsonPropertyName("station")] int? StationId
+);
+
+public sealed record StationSetResponse(
+    [property: JsonPropertyName("station")] IReadOnlyList<StationInfo> Station
+);
+
+public sealed record StationInfo(
+    [property: JsonPropertyName("id")] int Id,
+    [property: JsonPropertyName("name")] string Name
+);
